ebd40de1c17b5f003e5301230bcd800ef8d4a8ab
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-05 16:49:34 -0700

    added more validators to the user model

--- models/User.js
+++ models/User.js
@@ -1,18 +1,29 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+    minLength: [2, "The name must ba at least 2 characters long"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Incorrect email",
     },
-    email: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: [true, "User must have a password"],
-        minLength: [6, "Password cannot be shorter that 6 characters!"]
-    }
+    unique: [true, "This email is already in use. Please choose another one"],
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "User must have a password"],
+    minLength: [6, "Password cannot be shorter that 6 characters!"],
+    trim: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
