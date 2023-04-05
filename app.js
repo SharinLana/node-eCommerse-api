39fa644c3f29e63d7a4587f428500753269ffad4
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-05 16:30:37 -0700

    connected MongoDB to the server

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 const express = require("express");
-const app = express();
 
-const port = process.env.PORT || 5000;
+const app = express();
+dotenv.config();
+mongoose.set("strictQuery", true);
+
+const port = process.env.PORT || 5100;
 
 const start = async () => {
   try {
+    await mongoose
+      .connect(process.env.MONGODB_URL)
+      .then(() => console.log("MONGODB CONNECTED!"))
+      .catch((err) => console.log(err));
+
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
