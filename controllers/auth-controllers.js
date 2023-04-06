1a2ac7e0fb9ee127651d7a2d1473d02417241928
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-06 16:26:48 -0700

    set the admin role to the very first registered user. All the following  accounts will be automatically defined as user

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -8,12 +8,17 @@ const register = async (req, res) => {
     throw new BadRequestError("Please provide the name, email and password!");
   }
 
-  const emailAlreadyExists = await User.findOne({email});
+  const emailAlreadyExists = await User.findOne({ email });
   if (emailAlreadyExists) {
-    throw new BadRequestError("This email is already in use. Please choose another one");
+    throw new BadRequestError(
+      "This email is already in use. Please choose another one"
+    );
   }
+  // first registered user is an admin
+  const isFirstAccount = (await User.countDocuments({})) === 0;
+  const role = isFirstAccount ? "admin" : "user";
 
-  const user = await User.create(req.body);
+  const user = await User.create({ email, name, password, role }); // to secure the role (by preventing the user to register as admin)
 
   res.status(StatusCodes.CREATED).json({
     user,
