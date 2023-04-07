1be2190c701df6204ce82d68f34c084c839dc3d9
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-06 17:33:42 -0700

    removed the token from the register controller response and store it in the cookies

--- controllers/auth-controllers.js
+++ controllers/auth-controllers.js
@@ -23,8 +23,14 @@ const register = async (req, res) => {
   const tokenPayload = { name: user.name, userId: user._id, role: user.role };
   const token = createJWT({ payload: tokenPayload });
 
+  const oneDay = 1000 * 60 * 60 * 24;
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    expires: new Date(Date.now() + oneDay),
+  });
+
   res.status(StatusCodes.CREATED).json({
-    token,
     user,
   });
 };
