b23a26c9db001f2580e69c223fea595aac8068a5
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-07 15:56:21 -0700

    set initial logic for the getAllUsers and getSingleUser controllers

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -1,11 +1,22 @@
 const { StatusCodes } = require("http-status-codes");
+const User = require("../models/User");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
+} = require("../errors/index");
 
 const getAllUsers = async (req, res) => {
-  res.send("Get all users");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.send("Get a user");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new NotFoundError(`No user with id: ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
