ffc708c25f17db9f060d80cdb2a28e2391c341d0
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-08 16:45:47 -0700

    finalized the checkPermissions function

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,10 +1,16 @@
-const { PermissionsDeniedError } = require("../errors/index");
+const { PermissionDeniedError } = require("../errors/index");
 
 // To prevent situation when a user can access routes that belong to other user
 const checkPermissions = (currentUser, currentUserId) => {
-  console.log(currentUser);
-  console.log(currentUserId);
-  console.log(typeof currentUserId); //object
+  //   console.log(currentUser); // { name: 'Helena', userId: '642f5864e73bdfeb51ba29eb', role: 'user'
+  //   console.log(currentUserId); // new ObjectId("642f5ddf2015fe0959c9c2f3")
+  //   console.log(typeof currentUserId); //object
+
+  if (currentUser.role === "admin") return;
+  if (currentUser.userId === currentUserId.toString()) return;
+  throw new PermissionDeniedError(
+    "You don't have permissions for such action!"
+  );
 };
 
 module.exports = checkPermissions;
