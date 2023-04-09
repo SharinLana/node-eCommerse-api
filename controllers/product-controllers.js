15a1b1f0a7ed3b792325cce7b2dc4b6d2a3731ea
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-08 17:43:41 -0700

    created basic structure of the product controllers

---
+++ controllers/product-controllers.js
@@ -0,0 +1,40 @@
+const { StatusCodes } = require("http-status-codes");
+const Product = require("../models/Product");
+const {
+  BadRequestError,
+  UnauthenticatedError,
+  NotFoundError,
+} = require("../errors/index");
+
+const createProduct = async (req, res) => {
+  res.send("Create product");
+};
+
+const getAllProducts = async (req, res) => {
+  res.send("Get all products");
+};
+
+const getSingleProduct = async (req, res) => {
+  res.send("Get single product");
+};
+
+const updateProduct = async (req, res) => {
+  res.send("Update product");
+};
+
+const deleteProduct = async (req, res) => {
+  res.send("Delete product");
+};
+
+const uploadImage = async (req, res) => {
+  res.send("Upload image");
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+};
