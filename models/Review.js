81e8e75313e7a230ddeb9cbf7b0c1f08c6a0bc07
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-10 15:29:56 -0700

    created a Review model

---
+++ models/Review.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, "Please rate the product"],
+    },
+    title: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide review title"],
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+      required: [true, "Please provide review text"],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+// The user can make only 1 review per product
+reviewSchema.index({ product: 1, user: 1 }, { unique: true });
+
+const Review = mongoose.model("Review", reviewSchema);
+
+module.exports = Review;
