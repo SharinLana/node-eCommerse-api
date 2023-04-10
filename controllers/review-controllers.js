6047a62e317ca571b0185aa0338c43fe4e30654a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-10 16:20:06 -0700

    finalized the createReview controller

--- controllers/review-controllers.js
+++ controllers/review-controllers.js
@@ -1,5 +1,6 @@
 const { StatusCodes } = require("http-status-codes");
 const Review = require("../models/Review");
+const Product = require("../models/Product");
 const {
   BadRequestError,
   UnauthenticatedError,
@@ -7,7 +8,28 @@ const {
 } = require("../errors/index");
 
 const createReview = async (req, res) => {
-  res.send("Create review");
+  const { product: productId } = req.body;
+
+  const isValidProduct = await Product.findOne({ _id: productId });
+  if (!isValidProduct) {
+    throw new NotFoundError("No such product in the database!");
+  }
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+  if (alreadySubmitted) {
+    throw new BadRequestError(
+      "You have already submitted a review for this product"
+    );
+  }
+
+  req.body.user = req.user.userId;
+
+  const review = await Review.create(req.body);
+
+  res.status(StatusCodes.CREATED).json({ review });
 };
 
 const getAllReviews = async (req, res) => {
