e2731314691ed6c1c7be8396bc5cb0847f845b3b
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-10 16:35:46 -0700

    changed the parameter name in the util function checkPermissions.js

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,13 +1,13 @@
 const { PermissionDeniedError } = require("../errors/index");
 
 // To prevent situation when a user can access routes that belong to other user
-const checkPermissions = (currentUser, currentUserId) => {
+const checkPermissions = (currentUser, sourseUserId) => {
   //   console.log(currentUser); // { name: 'Helena', userId: '642f5864e73bdfeb51ba29eb', role: 'user'
   //   console.log(currentUserId); // new ObjectId("642f5ddf2015fe0959c9c2f3")
   //   console.log(typeof currentUserId); //object
 
   if (currentUser.role === "admin") return;
-  if (currentUser.userId === currentUserId.toString()) return;
+  if (currentUser.userId === sourseUserId.toString()) return;
   throw new PermissionDeniedError(
     "You don't have permissions for such action!"
   );
