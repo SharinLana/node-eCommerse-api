c7263fd816873f5868a7af0e2e73377a43e122c5
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-12 14:19:21 -0700

    renamed singleOrderItemSchema

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const singleCartItemSchema = new mongoose.Schema({
+const singleOrderItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
@@ -30,7 +30,7 @@ const orderSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    orderItems: [singleCartItemSchema],
+    orderItems: [singleOrderItemSchema],
     status: {
       type: String,
       enum: ["pending", "failed", "paid"],
