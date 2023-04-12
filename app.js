13f0c8f9489a5456e2532a9b788efeace2dcebe7
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-04-11 20:35:03 -0700

    imported order router in app.js and setup app.use(/api/v1/orders, orderRouter);

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(fileUpload());
 const authRouter = require("./routes/auth-routes");
 const userRouter = require("./routes/user-routes");
 const reviewRouter = require("./routes/review-routes");
+const orderRouter = require("./routes/order-routes");
 const productRouter = require("./routes/product-routes");
 const notFound = require("./middleware/not-found");
 const errorHandler = require("./middleware/error-handler");
@@ -42,6 +43,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/products", productRouter);
 app.use("/api/v1/reviews", reviewRouter);
+app.use("/api/v1/orders", orderRouter);
 app.use(errorHandler);
 app.use(notFound);
 
